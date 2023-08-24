d3416cebb0c9f3218988222984c734ccb943d00e
James Meegan <James.Meegan@softrams.com>
2023-08-24 15:06:16 -0400

    navbar top

--- AngularFrontend/src/app/app.module.ts
+++ AngularFrontend/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AllPatientsComponent } from './all-patients/all-patients.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent,
